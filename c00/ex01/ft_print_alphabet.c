2c637cb038fcc4c81c72fa2e38241d8d08b04831
PORTOS Bruno <brnoportos@gmail.com>
2025-07-16 13:27:04 +0300

    ft_print_alphabet.c

--- c00/ex01/ft_print_alphabet.c
+++ c00/ex01/ft_print_alphabet.c
@@ -1,5 +1,10 @@
 #include <unistd.h>
 void ft_print_alphabet (void)
 {
-	write (1, &"abcdefjhijklmnopqrstuvwxyz", 26);
+	write (1, "abcdefjhijklmnopqrstuvwxyz", 26);
+}
+int main (int argc, char *argv[])
+{
+	ft_print_alphabet ();
+	return (0);
 }
