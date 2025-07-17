87baa8645957f1e8663e4979fbad3bb1bf3b3a97
PORTOS Bruno <brnoportos@gmail.com>
2025-07-17 21:19:34 +0300

    ft_print_reverse_alphabet.c

--- c00/ex02/ft_print_reverse_alphabet.c
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -1,5 +1,18 @@
 #include <unistd.h>
-void ft_print_reverse_alphabet (void)
+
+void	ft_putchar(char c)
 {
-	write (1, "zyxwvutsrqponmlkjihgfedcba", 26);
+	write(1, &c, 1);
+}
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		ft_putchar(c);
+		c--;
+	}
 }
