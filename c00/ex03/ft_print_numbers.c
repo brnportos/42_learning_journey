233e382a56ad982334793d4c18cd9680f0f8f17d
PORTOS Bruno <brnoportos@gmail.com>
2025-07-17 21:30:27 +0300

    ft_print_numbers.c

--- c00/ex03/ft_print_numbers.c
+++ c00/ex03/ft_print_numbers.c
@@ -1,5 +1,18 @@
 #include <unistd.h>
-void ft_print_numbers (void)
+
+void	ft_putchar(char c)
 {
-	write (1, "0123456789", 10);
+	write(1, &c, 1);
+}
+
+void	ft_print_numbers(void)
+{
+	char	c;
+
+	c = '0';
+	while (c <= '9')
+	{
+		ft_putchar(c);
+		c++;
+	}
 }
