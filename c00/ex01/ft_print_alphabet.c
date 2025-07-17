e3b88ec722489fb4001a9d6257dacf6689fecdd5
PORTOS Bruno <brnoportos@gmail.com>
2025-07-17 21:01:06 +0300

    ft_print_alphabet.c

--- c00/ex01/ft_print_alphabet.c
+++ c00/ex01/ft_print_alphabet.c
@@ -1,5 +1,18 @@
 #include <unistd.h>
-void ft_print_alphabet (void)
+
+void	ft_putchar(char c)
 {
-	write (1, "abcdefjhijklmnopqrstuvwxyz", 26);
+	write(1, &c, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	char	c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		ft_putchar(c);
+		c++;
+	}
 }
