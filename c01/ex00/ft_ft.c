05c9fe852b978bd19af5dc857c0efbf596c88a53
PORTOS Bruno <brnoportos@gmail.com>
2025-07-22 18:52:41 +0300

    ft_ft.c

--- c01/ex00/ft_ft.c
+++ c01/ex00/ft_ft.c
@@ -1,23 +1,6 @@
 #include <unistd.h>
-#include <stdio.h>
 
 void	ft_ft(int *nbr)
 {
-	int	a;
-
-	a = 42;
-	nbr = &a;
-	printf("%d", *nbr);
-}
-
-int	main(int argc, char *argv[])
-{
-	int	a;
-	int	*nbr;
-
-	(void) argc;
-	(void) argv;	
-	nbr = &a;
-	ft_ft(nbr);
-	return (0);
+	*nbr = 42;
 }
