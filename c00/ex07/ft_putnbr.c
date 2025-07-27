14ca8f8d8054bc54eab9d13eeabb8c38d93e1b98
PORTOS Bruno <brnoportos@gmail.com>
2025-07-27 10:08:03 +0300

    ft_putnbr.c

---
+++ c00/ex07/ft_putnbr.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	ft_putchar(nb / 10 + '0');
+	ft_putchar(nb % 10 + '0');
+}
+
+int	main(void)
+{
+	ft_putnbr(100);
+	return (0);
+}
